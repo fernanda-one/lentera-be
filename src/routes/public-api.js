3dc12e4d588af7b8be871c7f53f2d46518a4c705
fernanda-one <fernandaiqshal111@gmail.com>
2024-05-03 15:08:17 +0700

    feat: add url get detail and list positions and roles

--- src/routes/public-api.js
+++ src/routes/public-api.js
@@ -3,6 +3,8 @@ import userController from "../controller/user-controller.js";
 import * as categoryController from "../controller/category-controller.js";
 import * as subCategoryController from "../controller/sub-category-controller.js";
 import * as contentController from "../controller/content-controller.js";
+import * as roleController from "../controller/role-controller.js";
+import * as positionController from "../controller/position-controller.js";
 
 const publicRouter = new express.Router();
 publicRouter.post("/api/users", userController.register);
@@ -19,9 +21,21 @@ publicRouter.get("/api/categories/:id", categoryController.getDetailCategory);
 publicRouter.get("/api/categories", categoryController.getCategory);
 // Sub Category
 
-publicRouter.get("/api/sub_categories/:id", subCategoryController.getDetailSubCategory);
+publicRouter.get(
+  "/api/sub_categories/:id",
+  subCategoryController.getDetailSubCategory
+);
 publicRouter.get("/api/sub_categories", subCategoryController.getSubCategory);
 // Content
+
+publicRouter.get("/api/content/:id", contentController.getAllContents);
+publicRouter.get("/api/content", contentController.getAllContents);
+
+publicRouter.get("/api/roles/:id", roleController.getAllRoles);
+publicRouter.get("/api/roles", roleController.getAllRoles);
+
+publicRouter.get("/api/positions/:id", positionController.getAllPositions);
+publicRouter.get("/api/positions", positionController.getAllPositions);
 
 publicRouter.get("/api/content/:id", contentController.getAllContents);
 publicRouter.get("/api/content", contentController.getAllContents);
